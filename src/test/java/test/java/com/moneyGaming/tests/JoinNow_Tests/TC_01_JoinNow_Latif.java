d57cde9ec0adb29d5daef2e147286b62a99e29ad
Latif Kaya <latif.kayaa@outlook.com>
2022-05-31 02:49:27 +0600

    TC_01 Join now error message displayed test passed

--- src/test/java/test/java/com/moneyGaming/tests/JoinNow_Tests/TC_01_JoinNow_Latif.java
+++ src/test/java/test/java/com/moneyGaming/tests/JoinNow_Tests/TC_01_JoinNow_Latif.java
@@ -1,9 +1,10 @@
-package test.java.com.moneyGaming.tests;
+package test.java.com.moneyGaming.tests.JoinNow_Tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -36,13 +37,32 @@ public class TC_01_JoinNow_Latif {
 
         //3. Select a title value from the dropdown
         //Locate the title dropdown
-        Select select = new Select(driver.findElement(By.xpath("////select[@name='map(title)']")));
+        Select select = new Select(driver.findElement(By.xpath("//select[@name='map(title)']")));
         select.selectByVisibleText("Mr");
 
-        
+        //4. Enter your first name and surname in the form
 
+        WebElement firstNameInputField= driver.findElement(By.xpath("//input[@name='map(firstName)']"));
+        firstNameInputField.sendKeys("Madara");
 
+        WebElement lastNameInputField = driver.findElement(By.xpath("//input[@name='map(lastName)']"));
+        lastNameInputField.sendKeys("Uchiha");
 
+        //5. Check the tick box with text 'I accept the Terms and Conditions and certify that I am over the age of 18.
+        WebElement submitFormCheckBox= driver.findElement(By.xpath("//input[@name='map(terms)']"));
+        submitFormCheckBox.click();
+
+        WebElement joinNowButton_InSignUpPage= driver.findElement(By.xpath("//input[@value='Join Now!']"));
+        joinNowButton_InSignUpPage.click();
+
+        //7. Validate that a validation message with text ‘ This field is required’ appears under the date of birth box
+
+        WebElement errorMessageUnderTheDateOfBirthBox = driver.findElement(By.xpath("//label[@for='dob']"));
+        String expected_ErrorMessage= "This field is required";
+
+        String actual_ErrorMessage= errorMessageUnderTheDateOfBirthBox.getText();
+
+        Assert.assertEquals(actual_ErrorMessage, expected_ErrorMessage);
 
 
     }
